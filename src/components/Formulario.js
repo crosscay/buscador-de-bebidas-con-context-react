d010d80605ce732beb6cf47d9f7485d63b969ea5
crosscay <caicedocarlos0306@gmail.com>
2020-09-27 08:16:03 -0500

    Creando el Formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -4,10 +4,33 @@ const Formulario = () => {
     return ( 
         <form>
             <fieldset>
-                <legend>
-                    
-                </legend>
+                <legend>Busca bebidas por Categoría o Ingrediente</legend>
             </fieldset>
+            <div className="row mt-4">
+                <div className="col-md-4">
+                    <input
+                        name="nombre"
+                        className="form-control"
+                        type="text"
+                        placeholder="Buscar por Ingrediente"
+                    />
+                </div>
+                <div className="col-md-4">
+                    <select 
+                        className="form-control"
+                        name="categoria"
+                    >
+                        <option value="">-- Seleccione Categoría --</option>
+                    </select>
+                 </div>
+                 <div className="col-md-4">
+                    <input
+                        type="submit"
+                        className="btn btn-block btn-primary"
+                        value="Buscar Bebidas"
+                    />
+                </div>
+            </div>
         </form>
      );
 }
