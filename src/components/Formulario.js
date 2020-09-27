ef312327924a533e69a8b0f7d863e54f40065240
crosscay <caicedocarlos0306@gmail.com>
2020-09-26 19:52:09 -0500

    creando la app y primeros pasos

---
+++ src/components/Formulario.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Formulario = () => {
+    return ( 
+        <form>
+            <fieldset>
+                <legend>
+                    
+                </legend>
+            </fieldset>
+        </form>
+     );
+}
+ 
+export default Formulario; 
